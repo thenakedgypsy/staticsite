a3ad1b2a3627a4675e0ce1525db24c8272b490bd
thenakedgypsy <sam.chittenden@hotmail.com>
2024-06-27 17:04:05 +0100

    updated tests for htmlnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -7,27 +7,24 @@ class TestHTMLNode(unittest.TestCase):
         testList = ["one","two","three"]
         testDict = {"href": "https://www.google.com", "target" : "_blank"}
         node = HTMLNode("tagname", "contents here", testList, testDict)
-        if node.props_to_html() == 'href="https://www.google.com" target="_blank"':
-            return True
+        assert node.props_to_html() == f' href="https://www.google.com" target="_blank"'
+        
 
     def test_PropsToHTML(self):
         testList = ["one","two","three"]
         testDict = {"href": "https://www.google.com", "target" : "_blank", "someThirdThing": "sadness"}
         node = HTMLNode("tagname", "contents here", testList, testDict)
-        if node.props_to_html() == 'href="https://www.google.com" target="_blank" someThirdThing="sadness"':
-            return True
+        assert node.props_to_html() == ' href="https://www.google.com" target="_blank" someThirdThing="sadness"'
+        
 
     def test_prints(self):
         testList = ["one","two","three"]
         testDict = {"href": "https://www.google.com", "target" : "_blank"}
         node = HTMLNode("tagname", "contents here", testList, testDict)
-        if node.__repr__ == "HTMLNode(tagname, contents here, ['one', 'two', 'three'], {'href': 'https://www.google.com', 'target': '_blank'})":
-            return True
-            
-        
-
-
-
+        actualRepr = repr(node)
+        assert actualRepr == "HTMLNode(tagname, contents here, ['one', 'two', 'three'], {'href': 'https://www.google.com', 'target': '_blank'})"
+       
+  
 
 if __name__ == "__main__":
     unittest.main()
