dc2aee9abf5a03c747c1dad48399a4d6ba96a0d7
thenakedgypsy <sam.chittenden@hotmail.com>
2024-06-27 12:19:55 +0100

    added more htmlnode tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,14 +2,28 @@ import unittest
 from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
-    def test_propsToHTML(self):
+
+    def test_simplePropsToHTML(self):
         testList = ["one","two","three"]
         testDict = {"href": "https://www.google.com", "target" : "_blank"}
         node = HTMLNode("tagname", "contents here", testList, testDict)
         if node.props_to_html() == 'href="https://www.google.com" target="_blank"':
             return True
 
-    
+    def test_PropsToHTML(self):
+        testList = ["one","two","three"]
+        testDict = {"href": "https://www.google.com", "target" : "_blank", "someThirdThing": "sadness"}
+        node = HTMLNode("tagname", "contents here", testList, testDict)
+        if node.props_to_html() == 'href="https://www.google.com" target="_blank" someThirdThing="sadness"':
+            return True
+
+    def test_prints(self):
+        testList = ["one","two","three"]
+        testDict = {"href": "https://www.google.com", "target" : "_blank"}
+        node = HTMLNode("tagname", "contents here", testList, testDict)
+        if node.__repr__ == "HTMLNode(tagname, contents here, ['one', 'two', 'three'], {'href': 'https://www.google.com', 'target': '_blank'})":
+            return True
+            
         
 
 
